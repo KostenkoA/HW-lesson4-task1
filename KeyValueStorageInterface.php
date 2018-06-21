47b0e4fa3aa0371f6a1e06c8ec6266adc4f62af8
KostenkoA <kostenkoam1212@gmail.com>
2018-06-21 15:29:46 +0300

    Added KeyValueStorageInterface.php

---
+++ KeyValueStorageInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Storage;
+
+
+interface KeyValueStorageInterface
+{
+    /**
+     * Store value by key.
+     *
+     * @param string $key
+     * @param mixed  $value
+     */
+    public function set($key, $value);
+
+    /**
+     * Gets value by key.
+     *
+     * @param string $key
+     */
+    public function get($key);
+
+    /**
+     * Check whether value is exist by key.
+     */
+    public function has($key);
+
+    /**
+     * Removes value by key.
+     *
+     * @param string $key
+     */
+    public function remove($key);
+
+    /**
+     * Clear storage.
+     *
+     * @param string $key
+     */
+    public function clear($key);
+}
